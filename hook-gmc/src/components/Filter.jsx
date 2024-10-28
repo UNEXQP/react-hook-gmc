44477a2f639a8dc6340041097034712c995df4b4
UNEXQP <anfelarotimi10@gmail.com>
2024-10-28 01:09:00 +0100

    hook-gmc

---
+++ hook-gmc/src/components/Filter.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Filter = ({ titleFilter, setTitleFilter, ratingFilter, setRatingFilter }) => {
+  return (
+    <div className="filter">
+      <input 
+        type="text" 
+        placeholder="Search by title" 
+        value={titleFilter}
+        onChange={(e) => setTitleFilter(e.target.value)}
+      />
+      <input 
+        type="number" 
+        placeholder="Minimum rating"
+        value={ratingFilter}
+        onChange={(e) => setRatingFilter(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default Filter;
